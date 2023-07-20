402c2b59788dfd6db0620d042f0781c2d54c2f3a
Luciano Borlenghi <lmborlenghi@hotmail.com>
2023-07-20 16:39:24 -0300

    Agregue una seccion contacto SIN formulario porque no logro hacerlo que envie el mail con el php

--- pages/form.php
+++ pages/form.php
@@ -4,7 +4,6 @@ $nombre = $_POST["nombre"];
 $mail = $_POST["email"];
 $asunto = $_POST["asunto"];
 $mensaje = $_POST["textarea"];
-
 $telefono = $_POST["telefono"];
 $website = $_POST["website"];
 
@@ -18,7 +17,7 @@ $mensaje .= "Su email es: " . $mail . ",\r\n";
 $mensaje .= "Asunto: " . $asunto . ",\r\n";
 $mensaje .= "telefono: " . $telefono . ",\r\n";
 $mensaje .= "website: " . $website . ",\r\n";
-$mensaje .= "Mensaje: " . $_POST["mensaje"] . ",\r\n";
+$mensaje .= "Mensaje: " . $_POST["textarea"] . ",\r\n";
 $mensaje .= "Enviado el: " . date('d/m/y', time());
 
 $para = "luchob@gmail.com";
